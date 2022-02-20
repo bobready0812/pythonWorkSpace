2e5d1eac7eada37e029969f57feb8d4614eea6a8
DaehyeonKim <Daehyeonoff@gmail.com>
2022-02-20 19:11:21 +0900

    20220220

--- practice.py
+++ practice.py
@@ -6,6 +6,13 @@ def deposit(balance, money):
     print("입금이 완료되었습니다. 잔액은 {0}원".format(balance + money))
     return balance + money
 
+def withdraw(balance, money):
+    if balance >= money:
+        print("출금 완료")
+    else: 
+        print("출금 실패")
+
 balance = 0
 balance = deposit(balance, 1000)
 print(balance)
+withdraw(1000, 500)
